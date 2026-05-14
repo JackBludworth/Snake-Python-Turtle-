5f4c693921d59316cdcde02d42ce39905ed11355
Your Name <JackBludworth@gmail.com>
2026-05-14 11:04:36 -0500

    done snake

--- snake.py
+++ snake.py
@@ -32,37 +32,40 @@ class Head(Turtle):
     screen.onkey(self.down,"s")
     screen.onkey(self.right,"d")
     screen.onkey(self.left,"a")
+    self.pu()
 
-def up(self):
-  if self.direction != "down":
-    self.setheading(90)
-    self.direction = "up"
-def down(self):
-  if self.direction != "up":
-    self.setheading(-90)
-    self.direction = "down"
+  def up(self):
+    if self.direction != "down":
+      self.setheading(90)
+      self.direction = "up"
+  def down(self):
+    if self.direction != "up":
+      self.setheading(-90)
+      self.direction = "down"
 
-def left(self):
-  if self.direction != "right":
-    self.setheading(180)
-    self.direction = "left"
+  def left(self):
+    if self.direction != "right":
+      self.setheading(180)
+      self.direction = "left"
 
-def right(self):
-  if self.direction != "left":
-    self.setheading(90)
-    self.direction = "right"
+  def right(self):
+    if self.direction != "left":
+      self.setheading(0)
+      self.direction = "right"
+      
+  def move(self):
+    self.forward(20)
+    if self.xcor() > 230 or self.xcor() < -230:
+      self.die()
+    if self.ycor() > 230 or self.ycor() < -230:
+      self.die()
 
-def move(self):
-  self.forward(5)
-  if self.xcor() > 230 or self.xcor() < -230:
-    self.die()
-  if self.ycor() > 230 or self.ycor() < -230:
-    self.die()
-    
-    
-def die(self):
-  self.ht()
-  self.alive = False
+  def die(self):
+    for g in body:
+      g.ht()
+    self.alive = False
+
+      
 
 
 class Segment(Turtle):
@@ -71,31 +74,56 @@ class Segment(Turtle):
     self.hideturtle()
     self.speed(0)
     self.color("green")
-    self.shape("circle")
+    self.shape("turtle")
     self.pu()
     self.goto(other.xcor(),other.ycor())
+    self.setheading(other.heading())
     self.st()
+    self.other = other
+    
 
-  def move(self, other):
-    pass
+  def move(self):
+    self.goto(self.other.xcor(),self.other.ycor())
+    self.setheading(self.other.heading())
+    
 
 class Apple(Turtle):
   def __init__(self):
     super().__init__()
-    pass
+    self.hideturtle()
+    self.speed(0)
+    self.color("red")
+    self.shape("square")
+    self.pu()
+    self.goto(random.randint(-200,200),random.randint(-200,200))
+    self.showturtle()
+
 
   def relocate(self):
-    pass
+    self.goto(random.randint(-200,200),random.randint(-200,200))
 
 
 def update():
-  if head.alive:
+  if head.alive == True:
     head.move()
+    for i in range(len(body)-1,0,-1):
+      body[i].move()
+    for t in range(4,len(body)):
+      if head.distance(body[t])<20:
+        head.die()
 
     if head.distance(apple) < 20:
       apple.relocate()
+      body.append(Segment(body[-1]))
+    
+    
 
-  screen.ontimer(update, 120)
+
+
+    
+      
+
+    screen.ontimer(update, 120)
 
 
 
@@ -103,18 +131,14 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(520,520)
 screen.listen()
-playing_area()
-head = Head(screen)
 screen.onkey(update, "space")
 
+playing_area()
+
+head = Head(screen)
 body = [head]
-
-
-screen.exitonclick()
-
-
-
-
+apple = Apple()
+update()
 
 
 screen.exitonclick()
